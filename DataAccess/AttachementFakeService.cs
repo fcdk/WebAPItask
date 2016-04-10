f11958dbac5b91d091d3c0a04e5ec65ca8158fa9
Maksym Hrytsaiuk <maksym_hrytsaiuk@epam.com>
2016-04-10 18:27:11 +0300

    Part of AttachementsConroller is implemented

---
+++ DataAccess/AttachementFakeService.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Core.Mapping;
+using System.Linq;
+using Epam.Sdesk.Model;
+
+namespace Epam.Sdesk.DataAccess
+{
+    public class AttachementFakeService
+    {
+        private readonly ICollection<Attachement> _mails = new List<Attachement>
+        {
+            new Attachement
+            {
+                Id = 1,
+                MailId = 1,
+                FileExtention = "rar",
+                StatusId = 1
+            },
+            new Attachement
+            {
+                Id = 2,
+                MailId = 1,
+                FileExtention = "jpg",
+                StatusId = 2
+            },
+            new Attachement
+            {
+                Id = 3,
+                MailId = 2,
+                FileExtention = "zip",
+                StatusId = 1
+            }
+        };
+
+        public IEnumerable<Attachement> GetAttachementsByMailId(int mailId)
+        {
+            return _mails.Where(x => x.MailId == mailId);
+        }
+
+        public Attachement GetAttachementsByMailIdAndAttachementId(int mailId, int attachementId)
+        {
+            return _mails.FirstOrDefault(x => x.MailId == mailId && x.Id == attachementId);
+        }
+
+        public Attachement GetAttachementsByMailIdAndAttachementIdAndFileExtention(int mailId, int attachementId, string fileExtention)
+        {
+            return _mails.FirstOrDefault(x => x.MailId == mailId && x.Id == attachementId && x.FileExtention == fileExtention);
+        }
+
+        public Attachement GetAttachementsByMailIdAndAttachementIdAndFileExtentionAndStatusId(int mailId, int attachementId, string fileExtention, int statusId)
+        {
+            return _mails.FirstOrDefault(x => x.MailId == mailId && x.Id == attachementId && x.FileExtention == fileExtention && x.StatusId == statusId);
+        }
+
+    }
+}
---
+++ SDSK.API/Controllers/AttachementsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Epam.Sdesk.DataAccess;
+using Epam.Sdesk.Model;
+
+namespace SDSK.API.Controllers
+{
+    public class AttachementsController : ApiController
+    {
+        private readonly AttachementFakeService _attachementService = new AttachementFakeService();
+
+        // GET api/mails/{id}/attachements
+        [Route("api/mails/{id}/attachements")]
+        public IEnumerable<Attachement> GetByMailId(int id)
+        {
+            return _attachementService.GetAttachementsByMailId(id);
+        }
+
+        // GET api/mails/{id}/attachements/{attId}
+        [Route("api/mails/{id}/attachements/{attId}")]
+        public Attachement GetByMailIdAndAttachementId(int id, int attId, string extention = null, int? status = null)
+        {
+            return _attachementService.GetAttachementsByMailIdAndAttachementId(id, attId);
+        }
+
+        //// GET api/mails/{id}/attachements/{attId}?extention={ext}
+        //[Route("api/mails/{id}/attachements/{attId}/extention={ext}")]
+        //public Attachement GetByMailIdAndAttachementIdAndFileExtention(int id, int attId, string ext)
+        //{
+        //    return _attachementService.GetAttachementsByMailIdAndAttachementIdAndFileExtention(id, attId, ext);
+        //}
+
+        //// GET api/mails/{id}/attachements/{attId}?extention={ext}?status={status}
+        //[Route("api/mails/{id}/attachements/{attId}/extention={ext}/status={status}")]
+        //public Attachement GetByMailIdAndAttachementIdAndFileExtentionAndStatusId(int id, int attId, string ext, int status)
+        //{
+        //    return _attachementService.GetAttachementsByMailIdAndAttachementIdAndFileExtentionAndStatusId(id, attId, ext, status);
+        //}
+
+    }
+}
