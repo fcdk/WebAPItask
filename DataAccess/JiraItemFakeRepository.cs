52614eda428004932303896f92a8a4b4bf525b73
Maksym Hrytsaiuk <maksym_hrytsaiuk@epam.com>
2016-04-10 23:07:47 +0300

    JiraItemController is implemented

---
+++ DataAccess/JiraItemFakeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Epam.Sdesk.Model;
+
+namespace Epam.Sdesk.DataAccess
+{
+    public class JiraItemFakeRepository
+    {
+        private readonly ICollection<JiraItem> _jiraItems = new List<JiraItem>
+        {
+            new JiraItem
+            {
+                JiraItemId = 1
+            },
+            new JiraItem
+            {
+                JiraItemId = 2
+            }
+        };
+
+        public JiraItem GetById(long id)
+        {
+            return _jiraItems.FirstOrDefault(x => x.JiraItemId == id);
+        }
+
+    }
+}
---
+++ SDSK.API/Controllers/JiraItemController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using Epam.Sdesk.DataAccess;
+using Epam.Sdesk.Model;
+
+namespace SDSK.API.Controllers
+{
+    public class JiraItemController : ApiController
+    {
+        private readonly JiraItemFakeRepository _jiraItemRepository = new JiraItemFakeRepository();
+
+        // GET api/jiraitems/{id}
+        [Route("api/jiraitems/{id}")]
+        public JiraItem Get(long id)
+        {
+            return _jiraItemRepository.GetById(id);
+        }
+
+        //GET api/jiraitems (must return the same as for GET api/jiraitems/1)
+        [Route("api/jiraitems")]
+        public JiraItem Get()
+        {
+            return Get(1);
+        }
+
+    }
+}
