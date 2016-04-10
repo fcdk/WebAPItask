c96aa21d78576c9b2a6aa88254410dc91c7cc7a3
Maksym Hrytsaiuk <maksym_hrytsaiuk@epam.com>
2016-04-10 17:04:16 +0300

    MailsController is implemented

---
+++ DataAccess/MailFakeService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Epam.Sdesk.Model;
+
+namespace Epam.Sdesk.DataAccess
+{
+    public class MailFakeService
+    {
+        private readonly ICollection<Mail> _mails = new List<Mail>
+        {
+            new Mail
+            {
+                Id = 1,
+                Sender = "lol@lol.com"
+            },
+            new Mail
+            {
+                Id = 2,
+                Sender = "ahah@ahaha.com"
+            }
+        };
+
+        public bool Add(Mail mail)
+        {
+            _mails.Add(mail);
+            return true;
+        }
+
+        public IEnumerable<Mail> GetAll()
+        {
+            return _mails;
+        }
+
+        public Mail GetById(long id)
+        {
+            return _mails.FirstOrDefault(x => x.Id == id);
+        }
+
+        public bool Update(long id, Mail mail)
+        {
+            var mailToUpdate = _mails.FirstOrDefault(x => x.Id == id);
+            if (mailToUpdate == null)
+                return false;
+
+            _mails.Remove(mailToUpdate);
+            _mails.Add(mail);            
+            return true;
+        }
+
+        public bool Delete(long id)
+        {
+            var mailToDelete = _mails.FirstOrDefault(x => x.Id == id);
+            if (mailToDelete == null)
+                return false;
+            _mails.Remove(mailToDelete);
+            return true;
+        }
+
+    }
+}
--- SDSK.API/Controllers/MailsController.cs
+++ SDSK.API/Controllers/MailsController.cs
@@ -1,9 +1,43 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Epam.Sdesk.DataAccess;
+using Epam.Sdesk.Model;
 
 namespace SDSK.API.Controllers
 {
     public class MailsController : ApiController
     {
+        private readonly MailFakeService _mailService = new MailFakeService();
+
+        // GET api/mails
+        public IEnumerable<Mail> GetAll()
+        {
+            return _mailService.GetAll();
+        }
+
+        // GET /api/mails/{id}
+        public Mail Get(long id)
+        {
+            return _mailService.GetById(id);
+        }
+
+        // PUT /api/mails/{id}
+        public bool Put(long id, Mail mail)
+        {
+            return _mailService.Update(id, mail);
+        }
+
+        // POST /api/mails
+        public bool Post(Mail mail)
+        {
+            return _mailService.Add(mail);
+        }
+
+        // DELETE /api/mails/{id}
+        public bool Delete(long id)
+        {
+            return _mailService.Delete(id);
+        }
 
     }
 }
